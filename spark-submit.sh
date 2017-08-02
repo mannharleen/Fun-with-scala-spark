cf5c3cd595c53f420f2b57b9480282259197e0dc
Harleen Mann <mannharleen@gmail.com>
2017-08-02 10:25:29 +0800

    Update and rename spark-submit.bash to spark-submit.sh

--- spark-submit.sh
+++ spark-submit.sh
@@ -2,5 +2,7 @@ spark-submit \
 --class wc \
 --master yarn \
 --deploy-mode cluster \  # can be client for client mode
---conf spark.ui.port = 22222 \
+--conf spark.ui.port = 22222 
+# -- jars #extra jars
+# --packages #extra packages
 wc_2.xxx.jar arg0 arg1
